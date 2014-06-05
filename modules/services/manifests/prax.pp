2fe6392bbe460255cdcf9e7571ad829df0e4cdb1
Terje Larsen <terlar@gmail.com>
2014-06-05 16:13:34 +0200

    services: prax: remove nss logic
    Replaced by dnsmasq and nginx.
    
    dnsmasq will handle the .dev domain resolve and nginx will handle the
    routing to prax.

--- modules/services/manifests/prax.pp
+++ modules/services/manifests/prax.pp
@@ -4,17 +4,5 @@ class services::prax {
     provider => 'git',
     source   => 'https://github.com/ysbaddaden/prax.git',
     revision => 'unstable'
-  } ->
-  file { '/usr/local/bin/praxd':
-    ensure => present,
-    source => '/opt/prax/install/initd',
-    mode   => '0755'
-  } ->
-  exec { 'install prax nss':
-    command => 'make install',
-    cwd     => '/opt/prax/ext',
-    creates => '/lib/libnss_prax.so.2'
   }
-
-  # put prax to end of /etc/nsswitch.conf
 }
