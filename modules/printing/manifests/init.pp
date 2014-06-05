64f2a2933767e413a1b29275d2bf086d7d98643b
Terje Larsen <terlar@gmail.com>
2014-06-05 18:29:58 +0200

    printing: cups: add service and config

--- modules/printing/manifests/init.pp
+++ modules/printing/manifests/init.pp
@@ -4,4 +4,23 @@ class printing {
   package { 'cups': }
   package { 'ghostscript': }
   aur::package { 'epson-inkjet-printer-escpr': }
+
+  file { '/etc/cups/cups-files.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'lp',
+    mode    => '0640',
+    source  => 'puppet:///modules/printing/cups-files.conf',
+    require => Package['cups']
+  }
+
+  service { 'cups':
+    ensure  => running,
+    enable  => true,
+    require => [
+      Package['cups'],
+      File['/etc/cups/cups-files.conf']
+    ]
+  }
+
 }
