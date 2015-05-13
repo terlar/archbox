2285b6b8ea22198494e82c4690b55c37fb683031
Terje Larsen <terlar@gmail.com>
2015-05-13 10:49:59 +0200

    gui: browser: update pentadactyl AUR name

--- modules/gui/manifests/browser.pp
+++ modules/gui/manifests/browser.pp
@@ -2,5 +2,5 @@ class gui::browser {
   package { 'firefox': }
   package { 'flashplugin': }
   aur::package { 'google-talkplugin': }
-  aur::package { 'pentadactyl-hg': }
+  aur::package { 'pentadactyl-git': }
 }
