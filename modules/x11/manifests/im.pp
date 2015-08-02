725d993d9c182be39efd889e3d9ab4f79abc469c
Terje Larsen <terlar@gmail.com>
2015-08-02 13:44:42 +0200

    x11: add ibus input manager

--- modules/x11/manifests/im.pp
+++ modules/x11/manifests/im.pp
@@ -1,4 +1,18 @@
 class x11::im {
-  package { 'fcitx-im': }
-  package { 'fcitx-configtool': }
+  package {
+    [
+      'fcitx-im',
+      'fcitx-configtool',
+    ]:
+  }
+
+  package {
+    [
+      'ibus',
+      'ibus-libpinyin',
+      'ibus-sunpinyin',
+      'ibus-m17n',
+      'm17n-db',
+    ]:
+  }
 }
