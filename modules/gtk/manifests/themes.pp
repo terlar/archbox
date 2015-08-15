ec795cde632cb7ac1c47d09238c9daa5c12b3427
Terje Larsen <terlar@gmail.com>
2015-08-15 18:02:18 +0200

    gtk: themes: add gnome standard

--- modules/gtk/manifests/themes.pp
+++ modules/gtk/manifests/themes.pp
@@ -1,4 +1,10 @@
 class gtk::themes {
-  package { 'numix-themes': }
+  package {
+    [
+      'gnome-themes-standard',
+      'numix-themes',
+    ]:
+  }
+
   aur::package { 'gtk-theme-numix-white': }
 }
