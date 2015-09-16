af6ac86975f97f08ee546494cd2785aee2dc1bae
Terje Larsen <terlar@gmail.com>
2015-09-16 12:12:54 +0200

    laptop: fix suspend

--- modules/laptop/manifests/macbook.pp
+++ modules/laptop/manifests/macbook.pp
@@ -13,4 +13,25 @@ class laptop::macbook inherits laptop {
   }
 
   aur::package { 'kbdlight': }
+
+  file { '/etc/systemd/logind.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/laptop/suspend-fix/logind.conf'
+  }
+
+  file { '/etc/sysctl.d/90-disable-swapiness.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/laptop/suspend-fix/90-disable-swapiness.conf'
+  }
+
+  file { '/etc/udev/rules.d/90-xhc_sleep.rules':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/laptop/suspend-fix/90-xhc_sleep.rules'
+  }
 }
