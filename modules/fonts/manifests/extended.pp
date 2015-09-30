6678d1a8485d3f7cadf49be5296a78e865d8442b
Terje Larsen <terlar@gmail.com>
2015-09-30 14:03:28 +0200

    fonts: add back wqy-zenhei

--- modules/fonts/manifests/extended.pp
+++ modules/fonts/manifests/extended.pp
@@ -3,6 +3,7 @@ class fonts::extended {
     [
       'ttf-linux-libertine',
       'ttf-symbola',
+      'wqy-zenhei',
       'adobe-source-han-sans-cn-fonts',
       'adobe-source-han-sans-jp-fonts',
       'adobe-source-han-sans-kr-fonts',
