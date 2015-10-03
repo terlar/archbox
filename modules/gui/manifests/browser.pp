aef4ee5c9b176cf849bee0fac16d937c37ee8432
Terje Larsen <terlar@gmail.com>
2015-10-03 10:53:37 +0200

    gui: browser: switch back to pentadactyl-git

--- modules/gui/manifests/browser.pp
+++ modules/gui/manifests/browser.pp
@@ -2,5 +2,5 @@ class gui::browser {
   package { 'firefox': }
   package { 'flashplugin': }
   aur::package { 'google-talkplugin': }
-  aur::package { 'firefox-pentadactyl': }
+  aur::package { 'pentadactyl-git': }
 }
