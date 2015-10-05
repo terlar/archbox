09983694cdbbf54f395bf9ec0ec127ef7c319f17
Terje Larsen <terlar@gmail.com>
2015-10-05 07:30:39 +0200

    fonts: add fontmatrix

--- modules/fonts/manifests/init.pp
+++ modules/fonts/manifests/init.pp
@@ -2,7 +2,12 @@ class fonts {
   include fonts::base
   include fonts::extended
 
-  aur::package { 'font-manager': }
+  aur::package {
+    [
+      'font-manager',
+      'fontmatrix-git',
+    ]:
+  }
   package { 'freetype2': }
   package { 'fontconfig': }
 }
