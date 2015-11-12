15c47f0d7b0f94328e83268eef6c783020de1263
Terje Larsen <terlar@gmail.com>
2015-11-13 00:32:11 +0100

    ssh: remove envoy which is not actively maintained anymore

--- modules/ssh/manifests/client.pp
+++ modules/ssh/manifests/client.pp
@@ -7,15 +7,4 @@ class ssh::client {
       'mosh',
     ]:
   }
-
-  package { 'envoy':
-    provider => 'aur',
-    name     => 'envoy-git',
-  }
-
-  service { 'envoy@gpg-agent.service':
-    ensure  => running,
-    enable  => true,
-    require => Package['envoy'],
-  }
 }
