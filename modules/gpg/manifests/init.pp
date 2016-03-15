a3e0e128e7c4343f795fecc5225f32de30e0b65d
Terje Larsen <terlar@gmail.com>
2016-03-15 04:59:31 +0100

    gpg: add yubikey related packages

--- modules/gpg/manifests/init.pp
+++ modules/gpg/manifests/init.pp
@@ -1,3 +1,13 @@
 class gpg {
   package { 'gnupg': }
+
+  package { 'yubikey-neo-manager':
+    provider => 'aur',
+    name     => 'yubikey-neo-manager-git',
+  }
+
+  package { 'yubikey-personalization-gui':
+    provider => 'aur',
+    name     => 'yubikey-personalization-gui-git',
+  }
 }
